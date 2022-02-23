c87399514673f4528e0488715d181a42df08b2a1
Lars <l.gessner@posteo.de>
2022-02-23 16:28:20 +0100

    modify catch error  message

--- openweather.js
+++ openweather.js
@@ -8,7 +8,7 @@ const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon
 fetch(url)
     .then(response => response.json())
     .then(data => printWeather(data))
-    .catch(e => console.error(e))
+    .catch(e => console.error("Something is wrong, please try it later - Error:", e))
 
 function printWeather(data) {
     console.log(`Waether in ${data.name}: ${data.weather[0].description} and ${data.main.temp}°C`)
