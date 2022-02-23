b9eb5d8e8ac851f6ba71152bd14067ef103427a9
Lars <l.gessner@posteo.de>
2022-02-23 15:45:15 +0100

    initial with gitignore

--- openweather.js
+++ openweather.js
@@ -1,5 +1,10 @@
 import fetch from "node-fetch"
+import key from "./apiKey.js"
 
-const baseUrl = 
+const latitude = 48.7701177843416
+const longitude = 9.169522057503494
+const url = `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`
 
-fetch()
+fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
